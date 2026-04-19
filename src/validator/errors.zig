2ca3b5a6424dfc76f5396cde883808b3673695c0
Andrew Kralovec <andrew.kralovec@yum.com>
2026-04-19 00:56:36 -0500

    feat(NonExecutableDefinition): Add ExecutableDefinitionsRule logic

--- src/validator/errors.zig
+++ src/validator/errors.zig
@@ -1,7 +1,24 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
-pub const ValidationErrorKind = enum { IGNORE };
+pub const ValidationErrorKind = enum {
+    // ExecutableDefinitionsRule
+    /// Executable definitions
+    ///
+    /// A GraphQL document is only valid for execution if all definitions are either
+    /// operation or fragment definitions.
+    ///
+    /// See https://spec.graphql.org/draft/#sec-Executable-Definitions
+    NonExecutableDefinition,
+    // LoneAnonymousOperationRule
+    /// Lone anonymous operation
+    ///
+    /// A GraphQL document is only valid if when it contains an anonymous operation
+    /// (the query short-hand) that it contains only that one operation definition.
+    ///
+    /// See https://spec.graphql.org/draft/#sec-Lone-Anonymous-Operation
+    MultipleAnonymousOperations,
+};
 
 // TODO: import the validation object in the future
 // for now, let it be a wrapper around the error kind.
@@ -9,7 +26,7 @@ pub const ValidationErrorKind = enum { IGNORE };
 pub const ValidationError = struct {
     kind: ValidationErrorKind,
 
-    pub fn init(kind: ValidationErrorKind) !ValidationError {
+    pub fn init(kind: ValidationErrorKind) ValidationError {
         return ValidationError{ .kind = kind };
     }
 
