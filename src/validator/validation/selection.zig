174e07471fd461db9e6ca1d16ce463dbdb04aac2
Andrew Kralovec <andrew.kralovec@yum.com>
2026-05-26 22:18:10 -0500

    chore: validateSelectionSet place holder

--- src/validator/validation/selection.zig
+++ src/validator/validation/selection.zig
@@ -1,10 +1,15 @@
 const std = @import("std");
 const ast = @import("../../graphql.zig").ast;
-const Validator = @import("../validator.zig").Validator;
+const DiagnosticList = @import("../validator.zig").DiagnosticList;
+const ExecutableDocument = @import("../validator.zig").ExecutableDocument;
 
 pub fn validateSelectionSet(
-    ctx: *Validator,
+    ctx: *DiagnosticList,
+    exec_doc: *const ExecutableDocument,
+    selection_set: ast.SelectionSetNode,
 ) !void {
     _ = ctx;
+    _ = exec_doc;
+    _ = selection_set;
     // TODO: add validation logic
 }
