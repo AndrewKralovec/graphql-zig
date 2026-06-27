6a87ca652bc06fe05c3fafe17fa2f543fe050543
Andrew Kralovec <andrew.kralovec@yum.com>
2026-06-26 22:00:29 -0500

    feat: enum

--- src/validator/validation/enum.zig
+++ src/validator/validation/enum.zig
@@ -1,21 +1,62 @@
 const std = @import("std");
 const ast = @import("../../graphql.zig").ast;
-const Validator = @import("../validator.zig").Validator;
+const DiagnosticList = @import("../validator.zig").DiagnosticList;
+const Schema = @import("../validator.zig").Schema;
+const validateDirectives = @import("./directive.zig").validateDirectives;
+const validateTypeSystemName = @import("../schema/validation.zig").validateTypeSystemName;
 
-pub fn validateEnumDefinition(ctx: *Validator, enum_def: ast.EnumTypeDefinitionNode) anyerror!void {
-    _ = ctx;
-    _ = enum_def;
-    // TODO: add validation logic
+pub fn validateEnumDefinition(
+    allocator: std.mem.Allocator,
+    diagnostics: *DiagnosticList,
+    schema: *const Schema,
+    enum_def: ast.EnumTypeDefinitionNode,
+) !void {
+    try validateTypeSystemName(diagnostics, enum_def.name, "an enum type");
+
+    try validateDirectives(
+        allocator,
+        diagnostics,
+        schema,
+        enum_def.directives,
+        .Enum,
+        &[_]ast.VariableDefinitionNode{},
+    );
+
+    // validate there is at least one enum value on the enum type
+    // https://spec.graphql.org/draft/#sel-DAHfFVFBAAEXBAAh7S
+    const values = enum_def.values orelse {
+        try diagnostics.push(.EmptyValueSet);
+        return;
+    };
+
+    if (values.len == 0) {
+        try diagnostics.push(.EmptyValueSet);
+        return;
+    }
+
+    for (values) |enum_val| {
+        try validateEnumValue(allocator, diagnostics, schema, enum_val);
+    }
 }
 
-fn validateEnumValue(ctx: *Validator, enum_val: ast.EnumValueDefinitionNode) anyerror!void {
-    _ = ctx;
-    _ = enum_val;
-    // TODO: add validation logic
-}
+fn validateEnumValue(
+    allocator: std.mem.Allocator,
+    diagnostics: *DiagnosticList,
+    schema: *const Schema,
+    enum_val: ast.EnumValueDefinitionNode,
+) !void {
+    try validateTypeSystemName(
+        diagnostics,
+        enum_val.name,
+        "an enum value",
+    );
 
-pub fn validateEnumExtension(ctx: *Validator, enum_ext: ast.EnumTypeExtensionNode) anyerror!void {
-    _ = ctx;
-    _ = enum_ext;
-    // TODO: add validation logic
+    try validateDirectives(
+        allocator,
+        diagnostics,
+        schema,
+        enum_val.directives,
+        .EnumValue,
+        &[_]ast.VariableDefinitionNode{},
+    );
 }
