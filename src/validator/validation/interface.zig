69d4a1854ed23d20d3561edbe7054516a3d09ee6
Andrew Kralovec <andrew.kralovec@yum.com>
2026-06-27 00:22:19 -0500

    feat: object place holders

--- src/validator/validation/interface.zig
+++ src/validator/validation/interface.zig
@@ -1,17 +1,58 @@
 const std = @import("std");
 const ast = @import("../../graphql.zig").ast;
-const Validator = @import("../validator.zig").Validator;
+const DiagnosticList = @import("../validator.zig").DiagnosticList;
+const Schema = @import("../validator.zig").Schema;
 
 pub fn validateInterfaceDefinition(
-    ctx: *Validator,
+    allocator: std.mem.Allocator,
+    diagnostics: *DiagnosticList,
+    schema: *const Schema,
+    iface_def: ast.InterfaceTypeDefinitionNode,
 ) !void {
-    _ = ctx;
-    // TODO: add validation logic
+    _ = allocator;
+    _ = diagnostics;
+    _ = schema;
+    _ = iface_def;
+    // TODO: validateTypeSystemName, validateDirectives, validateFieldDefinitions
+    // TODO: check that the interface has at least one field (EmptyFieldSet)
+    // TODO: validate implemented interfaces (interfaces can implement interfaces per the 2021 spec)
 }
 
 pub fn validateImplementsInterfaces(
-    ctx: *Validator,
+    allocator: std.mem.Allocator,
+    diagnostics: *DiagnosticList,
+    schema: *const Schema,
+    obj: ast.ObjectTypeDefinitionNode,
 ) !void {
-    _ = ctx;
-    // TODO: add validation logic
+    _ = allocator;
+    _ = diagnostics;
+    _ = schema;
+    _ = obj;
+    // TODO: for each interface reference in obj.interfaces
+}
+
+pub fn validateImplementationFieldTypes(
+    allocator: std.mem.Allocator,
+    diagnostics: *DiagnosticList,
+    schema: *const Schema,
+    obj: ast.ObjectTypeDefinitionNode,
+) !void {
+    _ = allocator;
+    _ = diagnostics;
+    _ = schema;
+    _ = obj;
+    // TODO: for each interface implemented by obj, for each interface field
+}
+
+pub fn validateImplementationFieldArguments(
+    allocator: std.mem.Allocator,
+    diagnostics: *DiagnosticList,
+    schema: *const Schema,
+    obj: ast.ObjectTypeDefinitionNode,
+) !void {
+    _ = allocator;
+    _ = diagnostics;
+    _ = schema;
+    _ = obj;
+    // TODO: for each interface implemented by obj, for each interface field
 }
